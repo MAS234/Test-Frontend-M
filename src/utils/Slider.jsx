c71675447d044cdc24b19a59e4d7622f8e87869d
Miguel Martinez <miguel.martinez.angel.salazar@gmail.com>
2023-06-17 00:52:27 -0300

    Modifcacion de imagenes

--- src/utils/Slider.jsx
+++ src/utils/Slider.jsx
@@ -1,11 +1,15 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
 import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 import "../styles/Slider.css";
 
-import { Pagination } from "swiper";
+//IMAGENES
+import primera from "../assets/img/foto.png"
+import segunda from "../assets/img/foto (2).png"
+import tercera from "../assets/img/foto (3).png"
+
 
 function Slider() {
   return (
@@ -17,15 +21,9 @@ function Slider() {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide><img src={primera} alt="primera" /></SwiperSlide>
+        <SwiperSlide><img src={segunda} alt="primera" /></SwiperSlide>
+        <SwiperSlide><img src={tercera} alt="primera" /></SwiperSlide>
       </Swiper>
     </>
   )
